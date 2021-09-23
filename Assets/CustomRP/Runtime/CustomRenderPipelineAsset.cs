feeb257df859e2b265942c18b3012d152d9eafee
EricJRico <eric.j.rico@gmail.com>
2021-09-23 16:01:43 -0400

    initial commit for develop

---
+++ Assets/CustomRP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        throw new System.NotImplementedException();
+    }
+}
