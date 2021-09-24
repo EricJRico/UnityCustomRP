b36d96bcf738918fac77667b7032ff24fd237109
EricJRico <eric.j.rico@gmail.com>
2021-09-24 18:09:27 -0400

    finished tutorial 2 for Custom RP from catlikecoding

--- Assets/CustomRP/Runtime/CustomRenderPipeline.cs
+++ Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -5,11 +5,27 @@ using UnityEngine.Rendering;
 
 public class CustomRenderPipeline : RenderPipeline
 {
+    bool useDynamicBatching, useGPUInstancing;
+
+    public CustomRenderPipeline(
+        bool useDynamicBatching, 
+        bool useGPUInstancing, 
+        bool useSRPBatcher)
+    {
+        this.useDynamicBatching = useDynamicBatching;
+        this.useGPUInstancing = useGPUInstancing;
+        GraphicsSettings.useScriptableRenderPipelineBatching = true;
+    }
+
     CameraRenderer cameraRenderer = new CameraRenderer();
 
     protected override void Render(ScriptableRenderContext scriptableRenderContext, Camera[] cameras)
     {
         foreach (var camera in cameras)
-            cameraRenderer.Render(scriptableRenderContext, camera);
+            cameraRenderer.Render(
+                scriptableRenderContext,
+                camera,
+                useDynamicBatching,
+                useGPUInstancing);
     }
 }
